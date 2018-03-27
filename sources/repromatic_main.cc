31282e934821366a42b10b3519fec9c00cef6e40
ptrckr <github@ptrckr.tokyo>
2018-03-27 23:05:48 +0200

    Changed the creation of some alert messages

--- sources/repromatic_main.cc
+++ sources/repromatic_main.cc
@@ -43,42 +43,51 @@ void Sort(bool extended = false) {
   int folder_count = 0;
 
   int total_file_count = repromatic::GetItemCount(folder, file_system, "file_pdf");
-  int current_file_iteration = 0;
+  int current_pdf_file_iteration = 0;
+
+  repromatic::PageDictionary all_pages;
 
   repromatic::IterateFolder(file_system, folder, folder,
-                [&file_pdf_count, &folder_count, &status_monitor, &current_file_iteration, total_file_count](
+                [&file_pdf_count, &folder_count, &status_monitor, &current_pdf_file_iteration, total_file_count, &all_pages](
                   ASFileSys file_system,
                   ASPathName root,
                   ASPathName as_item_path,
                   std::string item_type) -> void {
     if (item_type == "file_pdf") {
-      if (status_monitor.progMon && status_monitor.progMon->setText) {
-        ASText pdf_file_name = ASTextNew();
-        if (ASFileSysGetNameFromPathAsASText(file_system, as_item_path, pdf_file_name) == 0) {
-          char *pdf_file_name_cstr = ASTextGetEncodedCopy(pdf_file_name, AVAppGetLanguageEncoding());
+      current_pdf_file_iteration += 1;
+      file_pdf_count += 1;
 
-          char status_monitor_message[48];
-          std::sprintf(status_monitor_message, "%3d%% @ %-40.40s", (100 / total_file_count * current_file_iteration), pdf_file_name_cstr);
+      if (status_monitor.progMon) {
+        if (status_monitor.progMon->setText) {
+          char *filename = ASFileSysDIPathFromPath(
+            file_system, as_item_path, ASFileSysAcquireParent(file_system, as_item_path)
+          );
 
-          ASfree(pdf_file_name_cstr);
+          std::string status_message(filename != nullptr ? filename : "Unknown filename");
+          status_message += " - " + std::to_string(current_pdf_file_iteration) + "/" + std::to_string(total_file_count);
 
-          status_monitor.progMon->setText(ASTextFromEncoded(status_monitor_message, AVAppGetLanguageEncoding()), status_monitor.progMonClientData);
+          ASfree(filename);
+
+          status_monitor.progMon->setText(
+            ASTextFromEncoded(status_message.c_str(), AVAppGetLanguageEncoding()),
+            status_monitor.progMonClientData
+          );
+        }
+
+        if (status_monitor.progMon->setCurrValue) {
+          status_monitor.progMon->setCurrValue(
+            100 / total_file_count * current_pdf_file_iteration,
+            status_monitor.progMonClientData
+          );
         }
       }
 
-      if (status_monitor.progMon && status_monitor.progMon->setCurrValue) {
-        status_monitor.progMon->setCurrValue(100 / total_file_count * current_file_iteration, status_monitor.progMonClientData);
-      }
-
-      current_file_iteration += 1;
-
-      file_pdf_count += 1;
-
-      repromatic::PageDictionary page_dict;
+      repromatic::PageDictionary file_pages;
       PDDoc my_doc = PDDocOpen(as_item_path, file_system, NULL, false);
       ASPathName pdfRoot = ASFileSysAcquireParent(file_system, as_item_path);
 
-      page_dict.AddPagesFrom(my_doc);
+      file_pages.AddPagesFrom(my_doc);
+      all_pages.AddPagesFrom(my_doc);
 
       ASText pdf_file_name = ASTextNew();
       if (ASFileSysGetNameFromPathAsASText(file_system, as_item_path, pdf_file_name) != 0) {
@@ -87,7 +96,7 @@ void Sort(bool extended = false) {
       }
 
       ASPathName sorted_path = ASFileSysCreatePathWithFolderName(file_system, pdfRoot, "Sorted");
-      ASPathName sorted_din_format_path = ASFileSysCreatePathWithFolderName(file_system, sorted_path, page_dict.GetDinKey().c_str());
+      ASPathName sorted_din_format_path = ASFileSysCreatePathWithFolderName(file_system, sorted_path, file_pages.GetDinKey().c_str());
       ASPathName complete_path = ASFileSysCreatePathWithFolderNameWithASText(file_system, sorted_din_format_path, pdf_file_name);
 
       if (ASFileSysCreateFolder(file_system, sorted_din_format_path, true) != 0) {
@@ -105,12 +114,12 @@ void Sort(bool extended = false) {
   AVDocClose(first_avdoc, true);
 
   std::stringstream alert;
-  alert << "Sorted `";
-  alert << ASFileSysDIPathFromPath(NULL, folder, NULL) << "'." << std::endl << std::endl;
-  alert << "• " << file_pdf_count << " pdf file(s)." << std::endl;;
-  alert << "• " << folder_count << " sub folder(s).";
+  alert << "Sorted `" << ASFileSysDIPathFromPath(NULL, folder, NULL) << "'." << std::endl;
+  alert << "• " << file_pdf_count << " PDF file" << (file_pdf_count > 1 ? "s" : "");
+  alert << " in " << folder_count << " sub folder" << (folder_count > 1 ? "s" : "") << "." << std::endl << std::endl;
+  alert << all_pages.dinWrapper.ToString(false);
 
-  AVAlert(ALERT_NOTE, alert.str().c_str(), "OK", NULL, NULL, true);
+  AVAlertNote(alert.str().c_str());
 }
 
 // Sort Callbacks
@@ -128,74 +137,70 @@ void Overview(bool extended = false) {
   if (avActiveDocument) {
     PDDoc pdActiveDocument = AVDocGetPDDoc(avActiveDocument);
     page_dict.AddPagesFrom(pdActiveDocument);
-    AVAlert(ALERT_NOTE, page_dict.dinWrapper.ToString(true).c_str(), "OK", NULL, NULL, true);
+    AVAlertNote(page_dict.dinWrapper.ToString(true).c_str());
   } else {
     AVOpenSaveDialogParamsRec params;
     memset(&params, NULL, sizeof(AVOpenSaveDialogParamsRec));
     params.size = sizeof(AVOpenSaveDialogParamsRec);
-
     ASFileSys file_system;
     ASPathName folder;
     if (!AVAppChooseFolderDialog(&params, &file_system, &folder)) return;
 
     // Open a file to be able to get a status monitor.
-    ASPathName first_file = repromatic::GetFirstPdfFileInFolder(file_system, folder);
-
     AVDoc first_avdoc = NULL;
-    if (first_file != NULL) {
+    ASPathName first_file = repromatic::GetFirstPdfFileInFolder(file_system, folder);
+    if (first_file != nullptr) {
       first_avdoc = AVDocOpenFromFile(first_file, file_system, NULL);
     }
-
     ASFileSysReleasePath(file_system, first_file);
-
     AVStatusMonitorProcsRec status_monitor = repromatic::acrobat_utils::GetStatusMonitor();
 
     int file_pdf_count = 0;
     int file_non_pdf_count = 0;
     int folder_count = 0;
 
-    int total_file_count = repromatic::GetItemCount(folder, file_system);
-    int current_file_iteration = 0;
+    int total_file_count = repromatic::GetItemCount(folder, file_system, "file_pdf");
+    int current_pdf_file_iteration = 0;
 
     repromatic::IterateFolder(file_system, folder, folder,
-      [&page_dict, &file_pdf_count, &file_non_pdf_count, &folder_count, &status_monitor, &current_file_iteration, total_file_count](
+      [&page_dict, &file_pdf_count, &file_non_pdf_count, &folder_count, &status_monitor, &current_pdf_file_iteration, total_file_count](
                     ASFileSys file_system,
                     ASPathName root,
                     ASPathName as_item_path,
                     std::string item_type) -> void {
       if (item_type == "file_pdf") {
-        if (status_monitor.progMon && status_monitor.progMon->setText) {
-          ASText pdf_file_name = ASTextNew();
-          if (ASFileSysGetNameFromPathAsASText(file_system, as_item_path, pdf_file_name) == 0) {
-            char *pdf_file_name_cstr = ASTextGetEncodedCopy(pdf_file_name, AVAppGetLanguageEncoding());
+        current_pdf_file_iteration += 1;
+        file_pdf_count += 1;
 
-            char status_monitor_message[48];
-            std::sprintf(status_monitor_message, "%3d%% @ %-40.40s", (100 / total_file_count * current_file_iteration), pdf_file_name_cstr);
+        if (status_monitor.progMon) {
+          if (status_monitor.progMon->setText) {
+            char *filename = ASFileSysDIPathFromPath(
+              file_system, as_item_path, ASFileSysAcquireParent(file_system, as_item_path)
+            );
 
-            ASfree(pdf_file_name_cstr);
+            std::string status_message(filename != nullptr ? filename : "Unknown filename");
+            status_message += " - " + std::to_string(current_pdf_file_iteration) + "/" + std::to_string(total_file_count);
 
-            status_monitor.progMon->setText(ASTextFromEncoded(status_monitor_message, AVAppGetLanguageEncoding()), status_monitor.progMonClientData);
+            ASfree(filename);
+
+            status_monitor.progMon->setText(
+              ASTextFromEncoded(status_message.c_str(), AVAppGetLanguageEncoding()),
+              status_monitor.progMonClientData
+            );
+          }
+
+          if (status_monitor.progMon->setCurrValue) {
+            status_monitor.progMon->setCurrValue(
+              100 / total_file_count * current_pdf_file_iteration,
+              status_monitor.progMonClientData
+            );
           }
         }
-
-        if (status_monitor.progMon && status_monitor.progMon->setCurrValue) {
-          status_monitor.progMon->setCurrValue(100 / total_file_count * current_file_iteration, status_monitor.progMonClientData);
-        }
-
-        current_file_iteration += 1;
-
-        file_pdf_count += 1;
 
         PDDoc my_doc = PDDocOpen(as_item_path, file_system, NULL, false);
         page_dict.AddPagesFrom(my_doc);
         PDDocClose(my_doc);
       } else if (item_type == "file_non_pdf") {
-        if (status_monitor.progMon && status_monitor.progMon->setCurrValue) {
-          status_monitor.progMon->setCurrValue(100 / total_file_count * current_file_iteration, status_monitor.progMonClientData);
-        }
-
-        current_file_iteration += 1;
-
         file_non_pdf_count += 1;
       } else if (item_type == "folder") {
         folder_count += 1;
@@ -210,11 +215,11 @@ void Overview(bool extended = false) {
 
     std::stringstream alert;
     alert << "Searched `" << ASFileSysDIPathFromPath(NULL, folder, NULL) << "'." << std::endl;
-    alert << "• " << file_pdf_count << " pdf file(s)." << std::endl;
-    alert << "• " << folder_count << " folder(s)." << std::endl << std::endl;
+    alert << "• " << file_pdf_count << " PDF file" << (file_pdf_count > 1 ? "s" : "");
+    alert << " in " << folder_count << " sub folder" << (folder_count > 1 ? "s" : "") << "." << std::endl << std::endl;
     alert << page_dict.dinWrapper.ToString(false);
 
-    AVAlert(ALERT_NOTE, alert.str().c_str(), "OK", NULL, NULL, true);
+    AVAlertNote(alert.str().c_str());
   }
 }
 
